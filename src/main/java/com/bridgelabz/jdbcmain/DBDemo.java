045fedc2fc3420e264a1bee95a28d94035336546
ktritsh7 <ktriesh7@gmail.com>
2021-07-18 16:38:45 +0530

    Welcome to Employee Payroll JDBC Program

---
+++ src/main/java/com/bridgelabz/jdbcmain/DBDemo.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.jdbcmain;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Service JDBC Program !!");
+    }
+}
